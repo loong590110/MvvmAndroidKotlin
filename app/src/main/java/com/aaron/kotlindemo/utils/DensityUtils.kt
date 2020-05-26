4b88bb2a8d45cacaa09351330e1ac21591178479
shizailong <shizailong@qiyi.com>
2020-05-26 19:28:24 +0800

    density util

--- app/src/main/java/com/aaron/kotlindemo/utils/DensityUtils.kt
+++ app/src/main/java/com/aaron/kotlindemo/utils/DensityUtils.kt
@@ -7,11 +7,12 @@ import android.util.TypedValue
  * Created by Developer Zailong Shi on 2020-01-06.
  */
 object DensityUtils {
-    fun dp2px(context: Context, dp: Float): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            dp,
-            context.resources.displayMetrics
-        ).toInt()
-    }
+    fun dp2px(context: Context?, dp: Float): Int =
+        context?.run {
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                resources.displayMetrics
+            ).toInt()
+        } ?: 0
 }
