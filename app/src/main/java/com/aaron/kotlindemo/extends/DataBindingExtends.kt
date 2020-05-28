863d9b1faed584ece66e8010a41b8e204289a879
shizailong <shizailong@qiyi.com>
2020-05-28 19:57:20 +0800

    data binding extends

--- app/src/main/java/com/aaron/kotlindemo/extends/DataBindingExtends.kt
+++ app/src/main/java/com/aaron/kotlindemo/extends/DataBindingExtends.kt
@@ -5,6 +5,14 @@ import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 
 /**
+ * build.gradle:
+ * android{
+ *      ...
+ *      dataBinding {
+ *          enabled true
+ *      }
+ * }
+ *
  * Created by Developer Zailong Shi on 2020/5/28.
  */
 inline fun <reified T : ViewDataBinding> ViewGroup.inflate(attach: Boolean = true): T {
