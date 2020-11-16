5c0eac6c083e57f2aca0523fc2d3a7ffe0324c5a
Julius <zailongshi2013@gmail.com>
2020-11-16 22:47:20 +0800

    LessAnimationDrawable.kt

--- app/src/main/java/com/julius/mytube/widgets/LessAnimationDrawable.kt
+++ app/src/main/java/com/julius/mytube/widgets/LessAnimationDrawable.kt
@@ -12,37 +12,37 @@ import android.util.SparseArray
 import java.util.concurrent.atomic.AtomicBoolean
 
 
-class LessAnimationDrawable<T> private constructor(
-    files: Array<T>,
+class LessAnimationDrawable private constructor(
+    frameCount: Int,
     duration: Int,
-    loader: ImageLoader<T>
+    loader: ImageLoader
 ) : AnimationDrawable() {
-    private val drawables: SparseArray<PlaceHolderDrawable<T>>
-    private val loader: ImageLoader<T>
+    private val drawables: SparseArray<AnnulusDrawable>
+    private val loader: ImageLoader
     private val callback: Callback
-    private val paused = AtomicBoolean()
-    private val handler: Handler = Handler(Looper.getMainLooper())
     private val callbackProxy: Callback
+    private val paused = AtomicBoolean()
+    private val handler = Handler(Looper.getMainLooper())
 
     private var compensate: Long = 0
 
     companion object {
-        fun <T> create(
-            files: Array<T>,
+        fun create(
+            frameCount: Int,
             duration: Int,
-            loader: ImageLoader<T>
-        ): LessAnimationDrawable<T> {
-            return LessAnimationDrawable(files, duration, loader)
+            loader: ImageLoader
+        ): LessAnimationDrawable {
+            return LessAnimationDrawable(frameCount, duration, loader)
         }
     }
 
     init {
-        require(files.isNotEmpty()) { "files length <= 0" }
+        require(frameCount > 0) { "frame count <= 0" }
         require(duration > 0) { "duration <= 0(ms)" }
         this.loader = loader
         drawables = SparseArray()
         callback = object : Callback {
-            override fun call(index: Int, drawable: Drawable?) {
+            override fun call(index: Int, drawable: Drawable) {
                 drawables[index].drawable = drawable
                 if (paused.get()) {
                     paused.set(false)
@@ -53,7 +53,7 @@ class LessAnimationDrawable<T> private constructor(
             }
         }
         callbackProxy = object : Callback {
-            override fun call(index: Int, drawable: Drawable?) {
+            override fun call(index: Int, drawable: Drawable) {
                 compensate = SystemClock.uptimeMillis() - compensate
                 if (Thread.currentThread() === handler.looper.thread) {
                     callback.call(index, drawable)
@@ -62,12 +62,12 @@ class LessAnimationDrawable<T> private constructor(
                 }
             }
         }
-        for (i in files.indices) {
-            drawables.put(i, PlaceHolderDrawable.create(files[i], i))
+        for (i in 0 until frameCount) {
+            drawables.put(i, AnnulusDrawable.create(i))
             if (i > 0) {
                 drawables[i].previous = drawables[i - 1]
                 drawables[i - 1].next = drawables[i]
-                if (i == files.size - 1) {
+                if (i == frameCount - 1) {
                     drawables[0].previous = drawables[i]
                     drawables[i].next = drawables[0]
                 }
@@ -79,13 +79,13 @@ class LessAnimationDrawable<T> private constructor(
         }
     }
 
-    private fun load(index: Int, file: T) {
+    private fun load(index: Int) {
         compensate = SystemClock.uptimeMillis()
-        loader.load(index, file, callbackProxy)
+        loader.load(index, callbackProxy)
     }
 
     @Deprecated(
-        "This method had been deprecated.",
+        message = "Deprecated", level = DeprecationLevel.ERROR, replaceWith =
         ReplaceWith("throw IllegalArgumentException(\"This method had been deprecated.\")")
     )
     override fun addFrame(frame: Drawable, duration: Int) {
@@ -94,38 +94,41 @@ class LessAnimationDrawable<T> private constructor(
 
     override fun run() {
         if (drawables.size() >= 3) {
-            if (current is PlaceHolderDrawable<*>) {
-                val drawable = current as PlaceHolderDrawable<*>
+            if (current is AnnulusDrawable) {
+                val drawable = current as AnnulusDrawable
                 drawable.previous!!.previous!!.drawable = null
             }
         }
-        if (current is PlaceHolderDrawable<*>) {
-            val drawable = (current as PlaceHolderDrawable<*>).next
+        if (current is AnnulusDrawable) {
+            val drawable = (current as AnnulusDrawable).next
             if (drawable!!.drawable == null) {
                 paused.set(true)
-                @Suppress("UNCHECKED_CAST")
-                load(drawable.index, drawable.file as T)
+                load(drawable.index)
                 return
             }
         }
         super.run()
     }
 
+    override fun scheduleSelf(what: Runnable, `when`: Long) {
+        super.scheduleSelf(what, `when` - compensate)
+    }
+
     override fun start() {
         if (drawables.size() > 0 && drawables[0].drawable == null) {
-            load(0, drawables[0].file)
+            load(0)
             return
         }
         super.start()
     }
 
-    private class PlaceHolderDrawable<T> private constructor(val file: T, val index: Int) :
+    private class AnnulusDrawable private constructor(val index: Int) :
         Drawable() {
         private var _alpha: Int = -1
         private var _colorFilter: ColorFilter? = null
 
-        var previous: PlaceHolderDrawable<T>? = null
-        var next: PlaceHolderDrawable<T>? = null
+        var previous: AnnulusDrawable? = null
+        var next: AnnulusDrawable? = null
         var drawable: Drawable? = null
             set(drawable) {
                 field = drawable
@@ -170,17 +173,17 @@ class LessAnimationDrawable<T> private constructor(
         }
 
         companion object {
-            fun <T> create(file: T, index: Int): PlaceHolderDrawable<T> {
-                return PlaceHolderDrawable(file, index)
+            fun create(index: Int): AnnulusDrawable {
+                return AnnulusDrawable(index)
             }
         }
     }
 
-    interface ImageLoader<T> {
-        fun load(index: Int, file: T, callback: Callback?)
+    interface ImageLoader {
+        fun load(index: Int, callback: Callback)
     }
 
     interface Callback {
-        fun call(index: Int, drawable: Drawable?)
+        fun call(index: Int, drawable: Drawable)
     }
 }
